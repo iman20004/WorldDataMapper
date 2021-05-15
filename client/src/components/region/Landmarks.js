90a2bd11fae22ea8ae556e661b6187ef27968e04
Iman Ali <60799230+iman20004@users.noreply.github.com>
2021-05-15 02:42:11 -0400

    child landmarks cannot be deleted or edited

--- client/src/components/region/Landmarks.js
+++ client/src/components/region/Landmarks.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 
 const Landmarks = (props) => {
+    const disabledButton = () => { }
+
+    let childLandmark = props.landmark.includes("-");
+
 
     return (
         <div className='landmark-entry'>
             <div className='info-spacer'></div>
-            <i className="material-icons close-landmark" onClick={() => props.setShowDeleteLandmark(props.region, props.landmark)}>close</i>
+            <i className={childLandmark ? "material-icons close-landmark-disabled" : "material-icons close-landmark"}
+            onClick={childLandmark ? disabledButton : () => props.setShowDeleteLandmark(props.region, props.landmark)}>
+                close
+            </i>
             <div className='info-spacer'></div>
-            <div className='landmark-text' onClick={() => props.setShowEditLandmark(props.region, props.landmark)}>{props.landmark}</div>
+            <div className={childLandmark ? "landmark-text-disabled" : 'landmark-text'}
+            onClick={childLandmark ? disabledButton : () => props.setShowEditLandmark(props.region, props.landmark)}>
+                {props.landmark}
+            </div>
         </div>
     );
 };
